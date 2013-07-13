ca95f6d3e1f9286e877fa8a3dc6eee11e55af876
Jose Mario Mastrangelo <josem.mastrangelo@sympatico.ca>
2013-07-13 21:03:28 +0000

    Update 3

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var index = Buffer(fs.readFileSync('index.html'), 'ascii');
+    var index = Buffer(fs.readFileSync('index.html'), 'utf-8');
     response.send(index.toString());
 });
 
